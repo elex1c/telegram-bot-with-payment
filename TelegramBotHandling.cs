9a2391b2d33097810936265e8fd2b030ff642816
lilhimik <gift228338@gmail.com>
2023-07-03 10:30:44 +0300

    TelegramBotHandling structure is created

--- TelegramBotHandling.cs
+++ TelegramBotHandling.cs
@@ -1,11 +1,27 @@
 using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
 
 namespace TelegramBotWithPayment;
 
 public class TelegramBotHandling
 {
-    public async Task StartTelegramBotHandling(ITelegramBotClient botClient)
+    private CancellationToken CancellationToken { get; set; } = new CancellationToken();
+    
+    public Task StartTelegramBotHandling(ITelegramBotClient botClient)
+    {
+        botClient.StartReceiving(UpdateHandler, PollingErrorHandler, cancellationToken: CancellationToken);
+
+        return Task.CompletedTask;
+    }
+
+    async Task UpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
         
+    }
+
+    Task PollingErrorHandler(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
     }
 }
